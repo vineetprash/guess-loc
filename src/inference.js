90756b047bf609f444ec2ac30b3441a016f81aa3
Vineet <vineetprashant1@gmail.com>
2025-03-13 20:30:01 +0530

    local model

--- src/inference.js
+++ src/inference.js
@@ -75,7 +75,7 @@ export class ImageInferenceHandler {
 
     if (this.session === null) {
       this.session = await ort.InferenceSession.create(
-        "https://pub-019cfa8857ec42a6a0135120aa585246.r2.dev/exports/s2cell_ml_tvit_release0.onnx",
+        "./s2cell_ml_tvit_release0.onnx",
         { executionProviders: ["wasm"], graphOptimizationLevel: "all" }
       );
     }
